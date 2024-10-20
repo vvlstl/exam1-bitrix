3741fd96475d44325ae975eedd1899690b62f28a
artyem.stepanov <artemstepanov0114@gmail.com>
2024-10-20 14:45:42 +0700

    + Задание ex1-1

--- .top.menu.php
+++ .top.menu.php
@@ -1,4 +1,3 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $aMenuLinks = Array(
 	Array(
@@ -30,8 +29,15 @@ $aMenuLinks = Array(
 		"" 
 	),
 	Array(
-		"Контакты", 
-		"contacts/", 
+		"", 
+		"", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Партнерам", 
+		"/partneram/", 
 		Array(), 
 		Array(), 
 		"" 
